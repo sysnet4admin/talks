a715fe32c3dca27b64d9156e730fa0d03ada9200
Hoon Jo <pagaia@hotmail.com>
2025-10-06 20:18:40 +0900

    fix _talks

--- KubeCon/2025-NA/DEMO/k8s-cluster-builder/controlplane_node.sh
+++ KubeCon/2025-NA/DEMO/k8s-cluster-builder/controlplane_node.sh
@@ -21,7 +21,7 @@ kubectl completion bash > /etc/bash_completion.d/kubectl
 # alias kubectl to k 
 echo 'alias k=kubectl'               >> ~/.bashrc
 echo "alias ka='kubectl apply -f'"   >> ~/.bashrc
-echo "alias kd='kubectl delete'"   >> ~/.bashrc
+echo "alias kd='kubectl delete -f'"   >> ~/.bashrc
 echo "alias kg-po-ip-stat-no='kubectl get pods -o=custom-columns=\
 NAME:.metadata.name,IP:.status.podIP,STATUS:.status.phase,NODE:.spec.nodeName'" \
                                      >> ~/.bashrc 
